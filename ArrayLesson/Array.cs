752d3fd87a9457d98b65d93598bae4638406e397
oleggolen <oleg-golenischev63@yandex.ru>
2019-10-28 18:15:16 +0700

    Initial commit

---
+++ ArrayLesson/Array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLesson
+{
+    public class Array
+    {
+        private int[] _array;
+        public int Length { get; private set; }
+        public Array()
+        {
+            Length = 0;
+            _array = new int[0];
+        }
+        public Array(int [] array)
+        {
+            Length = array.Length;
+            _array = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                _array[i] = array[i];
+        }
+    }
+}
