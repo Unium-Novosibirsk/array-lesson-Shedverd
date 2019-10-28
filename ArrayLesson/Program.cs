5f2539a5136f724847579a599c0104b4b273c39b
Константин Степанов <Shedverd@mail.ru>
2019-10-28 18:32:52 +0700

    Ввод массива через пробел

--- ArrayLesson/Program.cs
+++ ArrayLesson/Program.cs
@@ -10,6 +10,8 @@ namespace ArrayLesson
     {
         static void Main(string[] args)
         {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Array array = new Array(arr);
         }
     }
 }
